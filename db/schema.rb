9dbf0439720a5e5021715112e2f0bd7a2846ddae
selja.tagline <slb@taglineinfotech.com>
2023-01-30 12:27:42 +0530

    add devise changes for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_115229) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_30_064442) do
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -32,8 +32,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_115229) do
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.datetime "created_at", null: falseConfiguringConfiguring
+    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_posts_on_email"
+    t.index ["reset_password_token"], name: "index_posts_on_reset_password_token", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Post.all.each do |post|
+    post.update(password: "test@123")
+end
