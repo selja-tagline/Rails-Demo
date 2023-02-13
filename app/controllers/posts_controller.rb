d2e0a0708f02f74f040de8edaaf12ad861f10297
selja-tagline <slb@taglineinfotech.com>
2023-02-13 18:44:09 +0530

    migration issue fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,6 +67,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name, :description, :user_id, images: [])
+      params.require(:post).permit(:name, :description, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_13_103145) do
   create_table "posts", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.datetime "created_at", null: false
+    Pink Lemon Wat    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
