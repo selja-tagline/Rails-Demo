63ca6b38967a46439327d692291e630cfa8dd275
selja-tagline <slb@taglineinfotech.com>
2023-03-27 18:45:26 +0530

    add comment before destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
-
+    p "::::::::::: destroy action ::::::::::";
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
       format.json { head :no_content }
