21335766b3383d8ea72936c479f2477a94bc50f5
selja-tagline <slb@taglineinfotech.com>
2023-04-10 16:31:41 +0530

    fixed the bug routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :users do
     resources :posts
-    resources :evants
+    resources :events
     resources :articles
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
