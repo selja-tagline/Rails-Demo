938462a8658633cdece08ff87365a2eb8204a15b
selja-tagline <slb@taglineinfotech.com>
2023-04-11 18:50:07 +0530

    check some demo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+    validates :name, presence: true
     belongs_to :user
+
+    before_create { puts "Before create" }
+    before_save { puts "Before save" }
+    before_update { puts "Before update" }
+    before_validation { puts "Before validation" }
 end
