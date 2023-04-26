bea93699bd7f64c46fcb19d481ed9eeba5610631
selja-tagline <slb@taglineinfotech.com>
2023-04-26 16:17:00 +0530

    send welcome mail to new user using action mailer

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,14 +5,37 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    p ":::: call new action :::: "
+    build_resource
+    yield resource if block_given?
+    respond_with resource
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    p ":::: call create action :::: "
+    build_resource(sign_up_params)
+
+    resource.save
+    UserMailer.welcome_email(resource).deliver_now
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # GET /resource/edit
   # def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :posts
   resources :users do
     resources :posts
     resources :events
     resources :articles
   end
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
 
   devise_scope :users do
+    get '/users/new', to: 'devise/registrations#new'
+    get '/users/sign_up', to: 'devise/registrations#create'
     get '/users/sign_out', to: 'devise/sessions#destroy'
     get "/auth/facebook/callback", to: "users/omniauth_callbacks#facebook"
   end
